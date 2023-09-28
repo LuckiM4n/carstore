62efd56c027dff9065e70041c6111f6064bbdd0b
LuckiM4n <valdelucas13@gmail.com>
2023-09-27 22:00:20 -0300

    Adequado a parte 3 do PDF do professor; Pull request adequada.

--- src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -1,5 +1,6 @@
 package br.com.carstore.servlet;
-
+import br.com.carstore.dao.CarDao;
+import br.com.carstore.model.Car;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,14 +10,20 @@ import java.io.IOException;
 
 @WebServlet("/create-car")
 public class CreateCarServlet extends HttpServlet {
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String carName = request.getParameter("car-name");
 
         System.out.println(carName);
+        Car car = new Car(carName);
+        new CarDao().createCar(car);
 
         request.getRequestDispatcher("index.html").forward(request, response);
 
     }
+
 }
+
+
